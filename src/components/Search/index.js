305d6cc40bcd5608f2021c4faf4dd0d68f47366f
PanPPP <phitchaporn.pipa@gmail.com>
2021-08-30 20:59:43 +0700

    feat: add expand onclick and animation

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,23 @@
+import { useState } from 'react'
+import classnames from 'classnames'
+
+import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg'
+
 import * as SC from './search.style'
 
 const Search = () => {
-  return <SC.Container>Search</SC.Container>
+  const [expand, setExpand] = useState(false)
+
+  console.log(expand)
+
+  return (
+    <SC.Container
+      className={classnames({ expand })}
+      onClick={() => setExpand(!expand)}
+    >
+      <SearchIcon />
+    </SC.Container>
+  )
 }
 
 export default Search
--- src/components/Search/search.style.js
+++ src/components/Search/search.style.js
@@ -1,3 +1,24 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 4px;
+  border-bottom: 2px solid var(--white);
+  width: 90px;
+  padding: 12px;
+  cursor: pointer;
+  background-color: rgba(33, 83, 163, 0);
+  transition: width 1s, background-color 1s;
+
+  &.expand {
+    width: 300px;
+    background-color: rgba(33, 83, 163, 1);
+  }
+
+  svg {
+    fill: #fafafa;
+    float: right;
+    padding-right: 36px;
+  }
+`
