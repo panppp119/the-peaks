db3d3c0c893ac21144ec4fb17e24ce0a0e0e12c6
PanPPP <phitchaporn.pipa@gmail.com>
2021-08-30 21:33:08 +0700

    feat: add input

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -7,14 +7,35 @@ import * as SC from './search.style'
 
 const Search = () => {
   const [expand, setExpand] = useState(false)
+  const [query, setQuery] = useState('')
 
-  console.log(expand)
+  const handleChange = (e) => {
+    const value = e.target.value
+    setQuery(value)
+  }
+
+  const onBlur = (e) => {
+    if (query === '') {
+      setExpand(false)
+    }
+  }
 
   return (
     <SC.Container
       className={classnames({ expand })}
       onClick={() => setExpand(!expand)}
+      onBlur={onBlur}
     >
+      {expand && (
+        <input
+          type='text'
+          onChange={handleChange}
+          onClick={(e) => e.stopPropagation()}
+          placeholder='Search all news'
+          value={query}
+          onBlur={onBlur}
+        />
+      )}
       <SearchIcon />
     </SC.Container>
   )
