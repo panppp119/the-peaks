3601ee05843160752c7ed8f445fedc21b6e23e5e
PanPPP <phitchaporn.pipa@gmail.com>
2021-08-31 20:50:56 +0700

    chore: dispatch loading

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -23,23 +23,40 @@ const HomePage = () => {
     { type: 'news', name: 'Top Stories', pageSize: 5 },
     { type: 'sport', name: 'Sports', pageSize: 3 },
     { type: 'culture', name: 'Cultures', pageSize: 3 },
-    { type: 'lifeandstyle', name: 'Life and Styles', pageSize: 3 },
+    { type: 'lifeandstyle', name: 'Lifestyles', pageSize: 3 },
   ]
 
   useEffect(() => {
-    sections.map((section) => getList(section))
+    dispatch({ type: CONST.IS_LOADING, loading: true })
+
+    sections.map(async (section, index) => {
+      if (sections.length === index + 1) {
+        const data = (await getList(section)) || []
+        data.length > 0 &&
+          dispatch({ type: CONST.STOP_LOADING, loading: false })
+      } else {
+        getList(section)
+      }
+    })
   }, [])
 
   const getList = async (section) => {
-    dispatch({ type: CONST.IS_LOADING, loading: true })
-
     const { type, pageSize } = section
     const params = { section: type, pageSize, orderBy: 'newest' }
 
     const data = await getArticles(params)
 
     setArticles((oldArticles) => ({ ...oldArticles, [type]: data }))
-    dispatch({ type: CONST.STOP_LOADING, loading: false })
+
+    return data
+  }
+
+  const title = (type, name) => {
+    if (type === 'news') {
+      return <h1 style={{ marginBottom: 30 }}>{name}</h1>
+    } else {
+      return <h2 style={{ marginBottom: 30 }}>{name}</h2>
+    }
   }
 
   return (
@@ -49,7 +66,8 @@ const HomePage = () => {
 
         return (
           <div className='section' key={index} style={{ marginBottom: 50 }}>
-            <h1 style={{ marginBottom: 30 }}>{name}</h1>
+            {title(type, name)}
+
             {loading ? (
               <Loader />
             ) : (
