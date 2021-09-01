dd74e2ec6faf46806e676a9d823258925ed82443
PanPPP <phitchaporn.pipa@gmail.com>
2021-09-01 12:48:47 +0700

    style: add cursor pointer

--- src/components/BookmarkButton/index.js
+++ src/components/BookmarkButton/index.js
@@ -6,15 +6,15 @@ import { ReactComponent as BookmarkIcon } from 'assets/images/bookmark-icon.svg'
 const Button = styled('button')`
   display: flex;
   align-items: center;
+  font-size: 13px;
+  line-height: 22px;
+  cursor: pointer;
   background-color: var(--primary);
   color: var(--white);
   border: none;
   border-radius: 4px;
   padding: 5px 10px;
   margin-bottom: 20px;
-
-  font-size: 13px;
-  line-height: 22px;
 
   svg {
     margin-right: 10px;
