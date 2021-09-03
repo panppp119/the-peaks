ec67882dd73c2e9b1a190993aa6d65ad389fb02b
PanPPP <phitchaporn.pipa@gmail.com>
2021-09-04 00:02:47 +0700

    feat: add bookmark to localstorage

--- src/contexts/bookmarkContext.js
+++ src/contexts/bookmarkContext.js
@@ -3,15 +3,17 @@ import { createContext, useState, useEffect, useMemo } from 'react'
 export const BookmarkContext = createContext([])
 
 const BookmarkContextProvider = (props) => {
-  const [bookmark, setBookmark] = useState([])
-
-  const value = useMemo(
-    () => ({ bookmark, setBookmark }),
-    [bookmark, setBookmark],
+  const [bookmark, setBookmark] = useState(
+    JSON.parse(localStorage.getItem('bookmark')),
   )
+
+  const value = useMemo(() => {
+    return { bookmark, setBookmark }
+  }, [bookmark, setBookmark])
 
   useEffect(() => {
     setBookmark(bookmark)
+    localStorage.setItem('bookmark', JSON.stringify(bookmark))
   }, [bookmark])
 
   return (
